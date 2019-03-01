22159ddedee31719e0cfbae0b1f455d8a8cede4b
curder <curder@foxmail.com>
2019-03-01 14:17:20 +0800

    添加部署时间

--- dingtalk.php
+++ dingtalk.php
@@ -5,13 +5,14 @@ namespace Deployer;
 use Deployer\Utility\Httpie;
 
 // Title of project
+
 set('dingtalk_title', function () {
     return get('application', 'Project');
 });
 
 // Webhook
 set('dingtalk_webhook', function () {
-    return 'https://oapi.dingtalk.com/robot/send?access_token='.get('dingtalk_access_token');
+    return 'https://oapi.dingtalk.com/robot/send?access_token=' . get('dingtalk_access_token');
 });
 
 // Deploy message
@@ -28,7 +29,7 @@ task('dingtalk:notify', function () {
         'msgtype' => 'markdown',
         'markdown' => [
             'title' => get('dingtalk_title'),
-            'text' => get('dingtalk_text'),
+            'text' => get('dingtalk_text') . sprintf(" \n\n\n > %s", date('Y-m-d H:i:s')),
         ],
         'isAtAll' => true,
     ];
@@ -47,7 +48,7 @@ task('dingtalk:notify:success', function () {
         'msgtype' => 'markdown',
         'markdown' => [
             'title' => get('dingtalk_title'),
-            'text' => get('dingtalk_success_text'),
+            'text' => get('dingtalk_success_text') . sprintf(" \n\n\n > %s", date('Y-m-d H:i:s')),
         ],
         'isAtAll' => true,
     ];
@@ -66,7 +67,7 @@ task('dingtalk:notify:failure', function () {
         'msgtype' => 'markdown',
         'markdown' => [
             'title' => get('dingtalk_title'),
-            'text' => get('dingtalk_failure_text'),
+            'text' => get('dingtalk_failure_text') . sprintf(" \n\n\n > %s", date('Y-m-d H:i:s')),
         ],
         'isAtAll' => true,
     ];
